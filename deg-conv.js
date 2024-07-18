343a62fcbd8cdf29ab3a88c45417d80fbf6074b4
Scott Parlane <scott@parlanenz.com>
2024-07-18 22:37:27 +1200

    Add degree minute seconds converters

--- deg-conv.js
+++ deg-conv.js
@@ -24,3 +24,32 @@ export function DMToDegrees (DM) {
   }
   return degs
 }
+
+export function degreesToDMS (degs, lat) {
+  let dir = ''
+  if (degs < 0) {
+    degs = degs * -1
+    dir = lat ? 'S' : 'W'
+  } else {
+    dir = lat ? 'N' : 'E'
+  }
+  const d = Math.floor(degs)
+  const mins = ((degs - d) * 60).toFixed(0)
+  const secs = ((degs - d - (mins / 60))).toFixed(1)
+
+  return d + ' ' + mins + ' ' + secs + ' ' + dir
+}
+
+export function DMSToDegrees (DMS) {
+  const parts = DMS.split(' ')
+  const d = parseInt(parts[0])
+  const mins = parseInt(parts[1])
+  const secs = parseFloat(parts[2])
+  const dir = parts[3]
+  const decmins = secs / 60
+  let degs = d + (mins + decmins) / 60
+  if (dir === 'S' || dir === 'W') {
+    degs = degs * -1
+  }
+  return degs
+}
