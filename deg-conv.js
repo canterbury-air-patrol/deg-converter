cf6cdf30c344024c17e21845e74e835d7b42a536
Scott Parlane <sjp@canterburyairpatrol.org>
2024-07-28 17:15:16 +1200

    Support all versions of DDD.ddd/DDD MM.mmm/DDD MM SS.sss input (#91)
    * Add degree minute seconds converters
    
    * Merge D/DM/DMS to degrees conversions into a single function
    
    In general be more tolerant of the input format
    * Allow the direction to be anywhere in the input
    * Accept as many space-seperated numbers as provided
    * Treat the space as the break between degrees minutes and seconds
    
    Also, add tests for this

--- deg-conv.js
+++ deg-conv.js
@@ -12,15 +12,52 @@ export function degreesToDM (degs, lat) {
   return d + ' ' + mins + ' ' + dir
 }
 
-export function DMToDegrees (DM) {
-  const parts = DM.split(' ')
-  const d = parseInt(parts[0])
-  const mins = parseFloat(parts[1])
-  const dir = parts[2]
-  const dec = mins / 60
-  let degs = d + dec
-  if (dir === 'S' || dir === 'W') {
+export function DMSToDegrees (DMS) {
+  let negative = false
+  if (DMS.includes('N')) {
+    DMS = DMS.replace('N', '')
+  } else if (DMS.includes('S')) {
+    negative = true
+    DMS = DMS.replace('S', '')
+  } else if (DMS.includes('E')) {
+    DMS = DMS.replace('E', '')
+  } else if (DMS.includes('W')) {
+    negative = true
+    DMS = DMS.replace('W', '')
+  }
+  const parts = DMS.split(' ')
+  let fract = 1
+  let p = 0
+  let degs = 0
+  while (p < parts.length) {
+    const value = parseFloat(parts[p])
+    if (!isNaN(value)) {
+      degs = degs + (value / fract)
+      fract = fract * 60
+    }
+    p++
+  }
+  if (negative) {
     degs = degs * -1
   }
   return degs
+}
+
+export function DMToDegrees (DM) {
+  return DMSToDegrees(DM)
+}
+
+export function degreesToDMS (degs, lat) {
+  let dir = ''
+  if (degs < 0) {
+    degs = degs * -1
+    dir = lat ? 'S' : 'W'
+  } else {
+    dir = lat ? 'N' : 'E'
+  }
+  const d = Math.floor(degs)
+  const mins = ((degs - d) * 60).toFixed(0)
+  const secs = ((degs - d - (mins / 60))).toFixed(1)
+
+  return d + ' ' + mins + ' ' + secs + ' ' + dir
 }
